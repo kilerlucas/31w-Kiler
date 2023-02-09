20e2215642abfeada3fd3d1fa73c0b6479428e3e
Eddy Martin <eddypmartin@gmail.com>
2023-02-09 16:27:29 -0500

    c4début front-page.php et 404.php

--- header.php
+++ header.php
@@ -8,10 +8,8 @@
     <?php wp_head(); ?>
 </head>
 <body>
-    <header>
-        <h1><?php  bloginfo('name'); ?></h1>
+    <header>       
+        <h1><a href="<?php  bloginfo('url'); ?>"><?php  bloginfo('name'); ?></a></h1> 
         <h2><?php  bloginfo('description'); ?></h2>
-        <h2><?php  bloginfo('url'); ?></h2>
-       <h1><a href="<?php  bloginfo('url'); ?>"><?php  bloginfo('name'); ?></a></h1> 
     </header>
     
