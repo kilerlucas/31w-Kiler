2b154f3a3fa0e0825aa9203ef8692004bfd3dcfc
Kiler <lucaskiler@gmail.com>
2023-03-01 10:26:04 -0500

    c9 ajouter menu dans aside

--- header.php
+++ header.php
@@ -23,4 +23,14 @@
         <h1><a href="<?php  bloginfo('url'); ?>"><?php  bloginfo('name'); ?></a></h1> 
         <h2><?php  bloginfo('description'); ?></h2>
     </header>
+    <aside class="site__aside">
+        <h3>Menu Secondaire</h3>
+        <?php 
+        
+        $category = get_queried_object();
+        wp_nav_menu(array(
+            "menu" => "cours", 
+            "container" => "nav" 
+        )); ?>
+    </aside>
     
