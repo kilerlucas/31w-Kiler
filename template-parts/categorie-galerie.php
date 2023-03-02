59acb3170bfc8183faa99f40aa45f1a814d9884c
Kiler <lucaskiler@gmail.com>
2023-03-02 16:42:01 -0500

    c10 ajoute de galerie

---
+++ template-parts/categorie-galerie.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * template part qui permettra d'affichier 
+ * un article provenant d'un conteneur de class blocflex 
+ * pour un article de categorie galerie
+ */
+
+?>
+<article class="blocflex__galerie">
+     <?php the_content(); ?>
+</article>
