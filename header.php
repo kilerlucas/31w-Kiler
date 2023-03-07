82546dfd296cf263e6bcb146af88e1500d0782cf
Kiler <lucaskiler@gmail.com>
2023-03-06 22:01:23 -0500

    c10 ajouté une section site__meta pour afficher les informations sur la publication : la date, l'auteur, la catégorie et les tags dans single.php

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php bloginfo( 'name' ); ?></title>
     <?php wp_head(); ?>
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/style.css">
 </head>
 <body <?php body_class( 'site' ); ?>>
     <header class="site__entete">  
@@ -37,7 +38,7 @@
         } else {
             $lemenu = "note-wp";
         }
-    wp_nav_menu( array(
+	    wp_nav_menu( array(
         'menu'       => $lemenu,
         'container'  => 'nav',
         'menu_class' => 'menu',
