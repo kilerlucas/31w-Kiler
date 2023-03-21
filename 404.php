40d54d21a1ba4b989cba4aa039a500bcfc51e757
Kiler <lucaskiler@gmail.com>
2023-03-20 21:55:03 -0400

    c14 ajoute plus de info sur page 404

--- 404.php
+++ 404.php
@@ -1,11 +1,9 @@
-<?php
-/**
-    Modèle index.php représente le modèle par défaut du thème
-*/
-get_header() ?>
+<?php get_header(); ?>
 <main class="site__main">
-    <code>404.php</code>
-    <h1>Erreur 404 - la page n'existe pas</h1>
-</main> 
+    <div class="container">
+        <h1 class="page-title">Erreur 404 - la page n'existe pas</h1>
+        <p>Nous sommes désolés, mais la page que vous avez demandée n'existe pas. Veuillez vérifier l'URL ou utiliser la fonction de recherche pour trouver ce que vous cherchez.</p>
+    </div>
+</main>
 <?php get_footer(); ?>
 
