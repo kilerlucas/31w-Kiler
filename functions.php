766ee61193e654a83a3bba2633d365ab3bee5e27
Kiler <lucaskiler@gmail.com>
2023-03-20 21:44:25 -0400

    c14 ajoute galerie dans le menu

--- functions.php
+++ functions.php
@@ -8,15 +8,29 @@ function ajouter_styles() {
 }
 add_action( 'wp_enqueue_scripts', 'ajouter_styles' );
 
-/* ------------------------------------------------- Enregistrement des menus */
+/* ----------------------------------- Add_theme_support */
 
+add_theme_support( 'html5', 
+                    array( 'search-form',  
+                           'gallery', 
+                           'caption' ) );
+
+add_theme_support( 'title-tag' );
+
+add_theme_support( 'custom-logo', 
+                    array(
+                        'height' => 150,
+                        'width'  => 150,
+                            ) );
+/*---------------------------------- Enregistrement des menus */
 function enregistrement_des_menus(){
     register_nav_menus( array(
-        'menu_entete' => 'Menu entête' ,
+        'menu_entete' => 'Menu entête',
         'menu_footer'  => 'Menu pied de page',
     ) );
 }
 add_action( 'after_setup_theme', 'enregistrement_des_menus', 0 );
+
 
 /**
  * Modifie la requete principale de Wordpress avant qu'elle soit exécuté
@@ -26,52 +40,31 @@ add_action( 'after_setup_theme', 'enregistrement_des_menus', 0 );
  * @param WP_query  $query la requête principal de WP
  */
 function cidweb_modifie_requete_principal( $query ) {
-    if (    $query->is_home() // si page d'accueil
-            && $query->is_main_query() // si requête principale
+    if (    $query->is_home() //si page d'accueil
+            && $query->is_main_query()  //si requête principale
             && ! is_admin() ) { // non tableau de bord
-      $query->set( 'category_name', 'note-wp' ); // filtre les articles de categorie «note-wp»
-      $query->set( 'orderby', 'title' );// trie selon le titre
-      $query->set( 'order', 'ASC' ); // en ordre ascendant
-      }
-     }
-     add_action( 'pre_get_posts', 'cidweb_modifie_requete_principal' );
+        $query->set( 'category_name', 'note-wp' ); // filtre les articles de categorie "note-wp"
+        $query->set( 'orderby', 'title' ); //trie selon se titre
+        $query->set( 'order', 'ASC' ); // en ordre ascendent 
+        }
+    }
+add_action( 'pre_get_posts', 'cidweb_modifie_requete_principal' );
 
 /**
- * Permet de modifier les titre du menu « cours »
+ * Permet de modifier les titres tu menu "cours"
  * @param $title : titre du choix menu
  * @param $item : le choix global
- * @param $args: Object qui représente la structure de menu 
- * 
+ * @param $args : Object qui représent la structure de menu
  */
-
-
-
-
-/* --------------------------------------------------- Add_theme_support */
-add_theme_support( 'html5', 
-                    array(  'search-form',
-                            'gallery', 
-                            'caption' 
-                    ) );
-
-add_theme_support( 'title-tag' );
-
-add_theme_support( 'custom-logo', 
-                    array(
-                        'height' => 150,
-                        'width'  => 150,
-                    ) );
-
 function perso_menu_item_title($title, $item, $args) {
-    // Remplacer 'nom_de_votre_menu' par l'identifiant de votre menu  
-  
+    // Remplacer 'nom_de_votre_menu' par l'identifiant de votre menu
     if($args->menu == 'cours') {
-    // Modifier la longueur du titre en fonction de vos besoins
-    $sigle = substr($title, 4,3);
-    $title = substr($title, 7);
-    $title = "<div class='cours__sigle'>" . $sigle . "</div>" . 
-              "<p class='cours__titre'>" . wp_trim_words($title, 2, ' ... ') . "</p>";
-    }
-    return  $title ;
+// Modifier la longueur du titre en fonction de vos besoins
+$sigle = substr ($title, 4, 3);
+$title = substr ($title, 7);
+$title = "<div class='cours__sigle'>" . $sigle . "</div>" . 
+            "<p class='cours__titre'>" . wp_trim_words($title, 2, ' ... ') . "</p>";
+}
+return $title ;
 }
 add_filter('nav_menu_item_title', 'perso_menu_item_title', 10, 3);
--- header.php
+++ header.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,29 +8,29 @@
     <title>Document</title>
     <?php wp_head(); ?>
 </head>
-<body class="site <?= (is_front_page() ? "no-aside" : "");  ?> ">
-    <header class="site__entete">  
+<body class="site <?= (is_front_page() ? "no-aside" : ""); ?> ">
+    <header class="site__entete">
         <section class="logomenu">
-            <?php the_custom_logo(); ?> 
+            <?php the_custom_logo(); ?>
             <div class="menusearch">
                 <input type="checkbox" id="chkBurger">
                 <?php wp_nav_menu(array(
                     'menu' => 'entete',
                     'container' => 'nav'
-                )); ?> 
+                )); ?>
                 <?= get_search_form(); ?>
                 <label for="chkBurger" class="burger">
-                <img src="https://s2.svgbox.net/hero-outline.svg?ic=menu&color=000" width="32" height="32">
-                </label>   
-            </div> 
+                <img src="https://s2.svgbox.net/hero-solid.svg?ic=menu&color=000" width="32" height="32">
+                </label>
+            </div>
         </section>
-        <h1><a href="<?php  bloginfo('url'); ?>"><?php  bloginfo('name'); ?></a></h1> 
-        <h2><?php  bloginfo('description'); ?></h2>
+        <h1><a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></h1>
+        <h2><?php bloginfo('description'); ?></h2> 
     </header>
-    <?php 
-    if (is_front_page() == false)
+    <?php
+    if(!is_front_page())
     {
-       get_template_part("template-parts/aside");
+     get_template_part("template-parts/aside");
     }
      ?>
-    
+     
