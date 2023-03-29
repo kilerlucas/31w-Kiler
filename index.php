e7547a04c9666c51f1d60742e5bd0c801e68609d
Kiler <lucaskiler@gmail.com>
2023-03-29 14:42:43 -0400

    c15 ajoute index.php dans h3

--- index.php
+++ index.php
@@ -8,10 +8,10 @@ get_header() ?>
 <?php 
 if (have_posts()):
     while (have_posts()) : the_post();
-        //the_title('<h1>','</h1>');
-        //the_permalink(); ?>
-        <h1><a href="<?php the_permalink(); ?>"><?= get_the_title();  ?> </a></h1>
-
+        // the_title('<h1>','</h1>');
+        // the_permalink(); ?>
+        <h1><a href="<?php the_permalink(); ?>"><?= get_the_title(); ?></a></h1>
+        
         <?php 
         // the_content();
         // the_excerpt();
@@ -21,4 +21,3 @@ endif;
 ?>   
 </main> 
 <?php get_footer(); ?>
-
--- single.php
+++ single.php
@@ -1,10 +1,12 @@
+
+
 <?php
 /**
     Modèle index.php représente le modèle par défaut du thème
 */
 get_header() ?>
-<main class="site__main">
-    <h3>single.php</h3>
+<main>
+    <h3>index.php</h3>
 <?php 
 if (have_posts()):
    while(have_posts()): the_post();
@@ -15,4 +17,3 @@ endif;
 ?>   
 </main> 
 <?php get_footer(); ?>
-
