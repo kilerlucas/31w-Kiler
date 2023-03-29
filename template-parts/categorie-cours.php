a0f7892683e4555fa237b1b81cdcf7af35a3f2b5
Kiler <lucaskiler@gmail.com>
2023-03-29 15:10:59 -0400

    c15 modifie template-parts

--- template-parts/categorie-cours.php
+++ template-parts/categorie-cours.php
@@ -2,7 +2,6 @@
 /**
  * template-part qui permettra d'afficher 
  * un article provenant d'un conteneur  de class blocflex
- * pour un article de catégorie cours
  */
 $titre = get_the_title();
 $sigle = substr($titre, 0, 7);
@@ -13,6 +12,5 @@ $duree = "90h";
     <h5><a href="<?php the_permalink(); ?>"> <?= $sigle; ?></a></h5>
     <h6><?= $titre_long ?></h6>
     <p><?= wp_trim_words(get_the_excerpt(), 15) ?></p>
-    <p><?php the_field('enseignant'); ?></p>
     <p><?= $duree ?></p>
 </article>
