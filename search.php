12edde1195bfe3bc703d4494ecdb00c98f982d66
kilerlucas <120747549+kilerlucas@users.noreply.github.com>
2023-04-02 21:07:08 -0300

    c18 modifications apportées à la page de résultats de recherche

--- search.php
+++ search.php
@@ -1,22 +1,27 @@
 <?php
 /**
-    Modèle search.php pour afficher les résultats de recherche
+Modèle search.php pour afficher les résultats de recherche
 */
 get_header() ?>
 <main class="site__main">
-    <code>search.php</code>
-    <section class="main__recherche">
+
 <?php 
-if (have_posts()):
-    while (have_posts()) : the_post();
-        //the_title('<h1>','</h1>');
-        //the_permalink(); ?>
-        <article>
+
+
+if (have_posts()): ?>
+    <h3>Résultats de recherche</h3>
+    <section class="blocflex">
+    <?php while (have_posts()) : the_post(); ?>
+        <article class="blocflex__article">
         <h5><a href="<?php the_permalink(); ?>"><?= get_the_title();  ?> </a></h5>
         <?= wp_trim_words(get_the_excerpt(), 30);?>
         </article>
-        <hr>
+    
     <?php endwhile;
+else: ?>
+    <img src="<?php echo get_template_directory_uri() ?>/img/empty.jpeg" alt="no results"/>
+    <h3>Aucun résultat pour votre recherche</h3>
+    <?php 
 endif; ?>  
 </section> 
 </main> 
