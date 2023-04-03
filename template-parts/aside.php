890a06f1b8d1172045d4d2ee30a09754c35db3b1
kilerlucas <120747549+kilerlucas@users.noreply.github.com>
2023-04-03 00:49:26 -0300

    c18 mettre à jour des pages individuelles, en modifiant la barre latérale en fonction du contenu

--- template-parts/aside.php
+++ template-parts/aside.php
@@ -1,14 +1,46 @@
+
+
 <aside class="site__aside">
+<?php if(! in_category('galerie')): ?>
+
+
+
         <h3>Menu Secondaire</h3>
         <?php 
-        $lemenu = 'note-wp';
-        if (in_category('cours'))
-        {$lemenu = 'cours';}
-        wp_nav_menu(array(
-            "menu" => $lemenu,
-            "container" => "nav"
-        )); 
+
+        if (!is_page()) {
+            $lemenu = 'note-wp';
+            if (in_category('cours'))
+            {$lemenu = 'cours';}
+            wp_nav_menu(array(
+                "menu" => $lemenu,
+                "container" => "nav"
+            )); 
+        } else {
+            $pages = get_pages(array(
+                'number' =>  3
+            ));
+            
+            ?>
+            <div class="menu-note-wp-container"><ul class="menu">
+            <?php
+            if($pages):
+                foreach($pages as $page):?>
+                    <li class="menu-item"><a class="latest-pages" href="/<?= $page->post_name; ?>"><?= $page->post_title; ?></a></li>
+                    <?php
+                endforeach;
+            endif;
+            ?>
+            </ul></div>
+            <?php
+        }
 
         ?>
+
+        <?php else: ?>
+
+            <h3>The <br />Galerie</h3>
+
+        <?php endif ?>
 
     </aside>
